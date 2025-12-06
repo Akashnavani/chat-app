3918ee9504cde5954eecad955301a1f63cb9de5a
AkashNavani <akashnavani25@gmail.com>
2025-12-06 23:43:50 +0530

    user and otp models

---
+++ backend/src/models/otp.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 300, // auto-delete after 5 minutes (300 seconds)
+  },
+});
+
+const Otp = mongoose.model("Otp", otpSchema);
+
+export default Otp;
